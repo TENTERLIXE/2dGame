bce900791e469e1a71deb19c76d3e5ca3e0365dd
ghMaxi <ec.qwertyui@gmail.com>
2021-07-01 11:45:28 +0300

    coin
    Поддержка AudioSource

--- Assets/Coin.cs
+++ Assets/Coin.cs
@@ -1,1 +1,4 @@
-using UnityEngine;public class Coin : MonoBehaviour{    public float value = 1;    public string resourceName = "Gold";    public void Collect()    {        Resources.Find(resourceName).Change(value);        Destroy(gameObject);    }}
+using UnityEngine;public class Coin : MonoBehaviour{    public float value = 1;    public string resourceName = "Gold";    private AudioSource _audioSource;    private void Start()
+    {
+        _audioSource = GetComponent<AudioSource>();
+    }    public void Collect()    {        Resources.Find(resourceName).Change(value);        _audioSource.Play();        GetComponent<Collider2D>().enabled = false;        GetComponentInChildren<MeshRenderer>().enabled = false;        Destroy(gameObject, 2);    }}
