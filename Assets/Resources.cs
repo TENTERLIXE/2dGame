94db2ede912b47ca33c01b4b80486e19cbcafc40
ghMaxi <ec.qwertyui@gmail.com>
2021-07-01 12:20:00 +0300

    save resource
    скрипт и префаб

--- Assets/Resources.cs
+++ Assets/Resources.cs
@@ -19,9 +19,18 @@ public class Resources : MonoBehaviour
     [SerializeField] private string m_ResourceName = "Gold";
     [SerializeField] private float m_Value = 0;
     [SerializeField] private float m_MaxValue = 9999;
+    [SerializeField] private bool save = false;
     public float GetMaxValue() => m_MaxValue;
     public UnityEvent OnZero, OnMax;
     public event Action<float> ChangeEvent;
+
+    private void Start()
+    {
+        resourceDict.TryGetValue(m_ResourceName, out var saved);
+        Change(saved);
+    }
+
+
     public void Change(float change)
     {
         m_Value = Mathf.Clamp(m_Value + change, 0, m_MaxValue);
@@ -33,5 +42,23 @@ public class Resources : MonoBehaviour
         {
             OnMax.Invoke();
         }
+    }
+
+
+    private static Dictionary<string, float> resourceDict
+        = new Dictionary<string, float>();
+    private void OnDestroy()
+    {
+        if (save)
+        {
+            if (resourceDict.ContainsKey(m_ResourceName))
+            {
+                resourceDict[m_ResourceName] = m_Value;
+            } else
+            {
+                resourceDict.Add(m_ResourceName, m_Value);
+            }
+        }
+        print(resourceDict[m_ResourceName]);
     }
 }
